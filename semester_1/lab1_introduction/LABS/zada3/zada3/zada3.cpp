bbe17ff7812ca17c730c59b74e89426b5722d516
TsimafeiFPMI <krupniktimofej@gmail.com>
2025-09-15 22:47:54 +0300

    sdelal

---
+++ semester_1/lab1_introduction/LABS/zada3/zada3/zada3.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+int main()
+{
+	setlocale(LC_ALL, "RU");
+	std::cout << "Введите число n: ";
+	int n;
+	std::cin >> n;
+	if (n < 1 || std::cin.fail()) {
+		std::cout << "Введено неправильное значение n";
+		exit(0);
+	}
+	std::cout << "Введите число m: ";
+	int m;
+	std::cin >> m;
+	if (m < 1 || std::cin.fail()) {
+		std::cout << "Введено неправильное значение m";
+		exit(1);
+	}
+	int c = 1;
+	while ((c <= n) && (c <= m)) {
+		if ((n % c == 0) && (m % c == 0)) {
+			std::cout << c << "\n";
+		}
+		c += 1;
+
+	}
+	return 0;
+}
