fd1dbd6ebc35981551a8c7f70e298d42a9c4e634
TsimafeiFPMI <krupniktimofej@gmail.com>
2025-09-15 21:20:47 +0300

    fff

---
+++ semester_1/lab1_introduction/LABS/zad2/zadanie2/zadanie2.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+int main()
+{
+	std::cout << "Введите число k";
+	int k;
+	std::cin >> k;
+	setlocale(LC_ALL, "RU");
+	if (k < 0 || std::cin.fail()) {
+		std::cout << "Введено неправильное значение k";
+		exit(0);
+	}
+	int a = 1;
+	int p1 = 1;
+	int b = 2;
+	int p2 = 1;
+	while (a <= k) {
+		p1 *= a;
+		a += 2;
+	}
+	if (p1 % 2 == 0) {
+		std::cout << "Ваш двойной факториал = " << p1 << std::endl;
+	}
+	else {
+		while (b <= k) {
+			p2 *= b;
+			b += 2;
+		}
+	}
+
+	return 0;
+}
