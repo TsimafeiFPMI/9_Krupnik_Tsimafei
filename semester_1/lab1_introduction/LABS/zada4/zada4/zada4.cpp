6d2e9a65180beb9c2466b4482e6eae9d91174110
TsimafeiFPMI <krupniktimofej@gmail.com>
2025-09-16 20:05:05 +0300

    ww

---
+++ semester_1/lab1_introduction/LABS/zada4/zada4/zada4.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+int main()
+{
+	setlocale(LC_ALL, "RU");
+	std::cout << "Введите число n: ";
+	int n;
+	int s = 0;
+	std::cin >> n;
+	if (n < 1 || std::cin.fail()) {
+		std::cout << "Введено неправильное значение n";
+		exit(0);
+	}
+	for (int i = 0; i <= n*2; i++) {
+		if (i % 2 != 0) {
+			s += i;
+		}
+	}
+	std::cout << "сумма ваших нечетных: " << s;
+
+	return 0;
+}
