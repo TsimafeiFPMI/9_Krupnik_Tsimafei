c788e7b49c7bb3c5482a9c8c535daba5576dada3
TsimafeiFPMI <krupniktimofej@gmail.com>
2025-09-24 10:12:01 +0300

    obnova

--- semester_1/lab2_integer_arithmetics/Labs/zada1/zada1/zada1.cpp
+++ semester_1/lab2_integer_arithmetics/Labs/zada1/zada1/zada1.cpp
@@ -5,32 +5,32 @@ int main() {
 	std::cout << "Введите значение a: ";
 	std::cin >> a;
 	if (std::cin.fail()) {
-		std::cout << "Введено неправильное значение a или b";
+		std::cout << "Введено неправильное значение a";
 		exit(0);
-	}
+	}                                                        //Проверка на букву или неправильное значение
 	int b;
 	std::cout << "Введите значение b: ";
 	std::cin >> b;
 	if (a <= b && std::cin.fail()) {
-		std::cout << "Введено неправильное значение a или b";
+		std::cout << "Введено неправильное значение b";
 		exit(0);
 	}
 	  for (int i = a; i <= b; i++) {
 		bool usl = false;
 		int n = abs(i);
 		while (n > 0 && !usl) {
-			int ost = n % 10;
+			int ost = n % 10;                                 //перебираем последнюю цифру 
 			int chislo = n / 10;
 			while (chislo > 0) {
-				if (chislo % 10 == ost) {
+				if (chislo % 10 == ost) {                     //проверяем на одинаковые цифры
 					usl = true;
 					break;
 				}
-				chislo /= 10;
+				chislo /= 10;                                 
 			}
 			n /= 10;
 		}
-		if (!usl) {
+		if (!usl) {                                           //если число подходит под условие мы его выводим
 			std::cout << i << " ";
 	  }
 	}
