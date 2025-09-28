c24b4c56166fd15fbafe98cf75b599b873fe99cc
TsimafeiFPMI <krupniktimofej@gmail.com>
2025-09-28 17:06:03 +0300

    zada1

---
+++ semester_1/lab3_pointers_and_arrays/Labs/zada1/zada1/zada1.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+int main() {
+	setlocale(LC_ALL, "RU");
+	int n;
+	std::cout << "Введите количество элементов ";
+	std::cin >> n;
+	if (std::cin.fail()) {
+		std::cout << "Введено недействительное количество элементов";
+		exit(1);
+	}
+	int array[1000] ;
+	for (int j = 0; j < n; j++) {
+		std::cin >> array[j];
+	}
+	int max = array[0];
+	for (int i = 1; i <= n; i++) {
+		if (array[i] > max) {
+			max = array[i];
+		}
+	}
+	std::cout << "макс число массива: " << max;
+
+
+
+
+
+
+	return 0;
+}
---
+++ semester_1/lab3_pointers_and_arrays/Labs/zada2/zada2/zada2.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <time.h>
+int main() {
+	setlocale(LC_ALL, "RU");
+	int n;
+	srand(time(NULL));
+	}
+	int *array[1000] = new array[1000;
+	for (int j = 0; j < n; j++) {
+		std::cin >> array[j];
+	}
+	int max = array[0];
+	for (int i = 1; i <= n; i++) {
+		if (array[i] > max) {
+			max = array[i];
+		}
+	}
+	std::cout << "макс число массива: " << max;
